c16b46885759c1c82206b393da16dd3693cb8853
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2018-08-14 23:43:16 +0500

    Recycler View for left side frame is done

---
+++ app/src/main/java/com/example/wajahat/foodbazaar/Word.java
@@ -0,0 +1,10 @@
+package com.example.wajahat.foodbazaar;
+public class Word {
+
+
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
