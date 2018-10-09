f6b1fc028c2a1eed2a408face2e1a8acb2628d0c
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2018-10-10 04:25:54 +0500

    Order Implementation

---
+++ app/src/main/java/com/example/wajahat/foodbazaar/OrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.wajahat.foodbazaar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity);
+    }
+}
