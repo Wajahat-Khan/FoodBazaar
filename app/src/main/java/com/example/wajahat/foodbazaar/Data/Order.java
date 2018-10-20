229b1f8b0bff9662231d5a92640f16c134878739
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2018-10-20 12:55:59 +0500

    Table No and SoftKey keyboard half screen implemented

--- app/src/main/java/com/example/wajahat/foodbazaar/Data/Order.java
+++ app/src/main/java/com/example/wajahat/foodbazaar/Data/Order.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 public class Order implements Serializable {
 
     private HashMap<Integer, Integer> order_list=new HashMap<>();
+    int table_no;
+    int order_no;
     public Order(){
 
     }
@@ -18,4 +20,19 @@ public class Order implements Serializable {
         this.order_list = order_list;
     }
 
+    public void setTable_no(int table_no) {
+        this.table_no = table_no;
+    }
+
+    public int getTable_no() {
+        return table_no;
+    }
+
+    public void setOrder_no(int order_no) {
+        this.order_no = order_no;
+    }
+
+    public int getOrder_no() {
+        return order_no;
+    }
 }
